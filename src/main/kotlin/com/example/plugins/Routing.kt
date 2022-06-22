f99970f6b3625f3e6abe85cbbbf6ebd654e74073
kelechi <oparaiskelechi@gmail.com>
2022-06-22 01:13:42 +0100

    router skeletal in check

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -9,8 +9,37 @@ import io.ktor.server.request.*
 fun Application.configureRouting() {
 
     routing {
+        // localhost:8080/home
         get("/") {
             call.respondText("Hello World!")
         }
+        // localhost:8080/note/id
+        get("/note/{id}") {
+            val id = call.parameters["id"] ?: return@get
+            call.respond(id)
+        }
+
+        // localhost:8080/notes?q=value
+        get("/note") {
+            val id = call.request.queryParameters["id"] ?: return@get
+            call.respond(id)
+        }
+
+        route("/notes") {
+            route("/create") {
+                // localHost:8080/notes/create
+                post {
+                    val body = call.receive<String>()
+                    call.respond(body)
+                }
+            }
+            // localHost:8080/notes/
+            delete {
+                val body = call.receive<String>()
+                call.respond(body)
+            }
+        }
+
+
     }
 }
--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -16,18 +16,18 @@ fun Application.configureSecurity() {
 
     authentication {
         jwt {
-            val jwtAudience = this@configureSecurity.environment.config.property("jwt.audience").getString()
-            realm = this@configureSecurity.environment.config.property("jwt.realm").getString()
-            verifier(
-                    JWT
-                            .require(Algorithm.HMAC256("secret"))
-                            .withAudience(jwtAudience)
-                            .withIssuer(this@configureSecurity.environment.config.property("jwt.domain").getString())
-                            .build()
-            )
-            validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
-            }
+//            val jwtAudience = this@configureSecurity.environment.config.property("jwt.audience").getString()
+//            realm = this@configureSecurity.environment.config.property("jwt.realm").getString()
+//            verifier(
+//                    JWT
+//                            .require(Algorithm.HMAC256("secret"))
+//                            .withAudience(jwtAudience)
+//                            .withIssuer(this@configureSecurity.environment.config.property("jwt.domain").getString())
+//                            .build()
+//            )
+//            validate { credential ->
+//                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+//            }
         }
     }
     data class MySession(val count: Int = 0)
